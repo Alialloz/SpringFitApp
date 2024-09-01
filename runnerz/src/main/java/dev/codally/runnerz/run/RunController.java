9b841deb9559fa341f39095238df62aea413f700
Alialloz <alialloz94450@gmail.com>
2024-09-01 16:14:09 -0400

    Implementation of RunController & RunRepository

---
+++ runnerz/src/main/java/dev/codally/runnerz/run/RunController.java
@@ -0,0 +1,17 @@
+package dev.codally.runnerz.run;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RunController {
+
+    private 
+
+    List<Run> findAll(){
+        return null;
+    }
+}
