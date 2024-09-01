7b8691ba0bec89d76f4caa28a49e866015fdd182
Alialloz <alialloz94450@gmail.com>
2024-09-01 15:30:50 -0400

    Implementation class Run & Main class for launching project

--- runnerz/src/main/java/dev/codally/runnerz/run/Run.java
+++ runnerz/src/main/java/dev/codally/runnerz/run/Run.java
@@ -2,6 +2,7 @@ package dev.codally.runnerz.run;
 
 import java.time.LocalDateTime;
 
+// Record est immuable, les getters setters et autres sont generees.
 public record Run(
         Integer id,
         String title,
